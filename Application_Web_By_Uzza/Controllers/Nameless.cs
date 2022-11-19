9797b74959972cd7013b26f22de9f1e7776043b5
Uzza <salamsyr12213@gmail.com>
2022-11-19 09:25:55 +0100

    Add project files.

---
+++ Application_Web_By_Uzza/Controllers/Nameless.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application_Web_By_Uzza.Controllers
+{
+    public class Nameless : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Project()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+    }
+}
